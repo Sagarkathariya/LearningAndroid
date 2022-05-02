3544f570e3f1218c507118bda02fec62f80301b5
Sagar Kathariya <sagarkathariya@gmail.com>
2022-05-02 08:54:12 +0545

    commit by Sagar

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    RadioButton genderradioButton;
+    RadioGroup radioGroup;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        radioGroup=(RadioGroup)findViewById(R.id.radioGroup);
+    }
+
+
+    public void onclickbuttonMethod(View v){
+        int selectedId = radioGroup.getCheckedRadioButtonId();
+        genderradioButton = (RadioButton) findViewById(selectedId);
+        if(selectedId==-1){
+            Toast.makeText(MainActivity.this,"Nothing selected", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(MainActivity.this,genderradioButton.getText(), Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
